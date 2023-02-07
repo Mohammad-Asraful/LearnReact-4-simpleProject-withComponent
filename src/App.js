d862dc12cfbdeefa3ce938bb18cb5d5d64495b2c
Mohammad-Asraful <asrafmail01@gmail.com>
2023-02-07 12:33:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import './index.css'
 
-function App() {
+const App = () => {
+  let currentDate = new Date(2023, 2, 3, 1)
+  currentDate = currentDate.getHours()
+  let greeting = ''
+  const cssstyle = {};
+
+  if (currentDate >= 1 && currentDate < 12) {
+    greeting = 'Good Morning'
+    cssstyle.color = 'green'
+  } else if (currentDate >= 12 && currentDate < 19) {
+    greeting = 'Good Afternoon'
+    cssstyle.color = 'orange'
+  } else {
+    greeting = 'Good Night'
+    cssstyle.color = 'black'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Sir, <span style={cssstyle}>{greeting}</span></h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
